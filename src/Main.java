605318d04ac514c606073579293f1f1cddf3d8e6
Julian Gödde <goedde@tuta.io>
2024-03-27 19:49:30 +0100

    Replace generate dummy calls with args from cli

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
-import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        List<Email> emails = SpamService.generateDummyMails();
-
         SpamService spamService = new SpamService();
+
+        ArrayList<Email> emails = IntStream.range(0, args.length)
+                .mapToObj(i -> new Email(i, args[i]))
+                .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
 
         spamService.getSpamProbabilities(emails).forEach((email, spamPercentage) ->
                 System.out.println(email.Content() + " - " + spamPercentage.toString() + " spam probability"));
